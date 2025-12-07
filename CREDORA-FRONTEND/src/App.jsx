16dbe5655c75aef50d26675792b700cdb252ba00
sharveshl <loganathansharvesh14@gmail.com>
2025-12-07 21:17:04 +0530

    fix the error  of routing

--- CREDORA-FRONTEND/src/App.jsx
+++ CREDORA-FRONTEND/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </Router>
   )
